c5c56a921f7d6629d73a7b65132318dcab737724
SyedaMuntahaShakir20 <muntahasyeda20@gmail.com>
2024-04-15 13:24:23 -0700

    push my calculator to github

---
+++ index.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer';
+
+const answers = await inquirer.prompt([
+{
+ name: "firstName",
+ message: "Enter your first number: ",
+ type: "number"
+},
+
+{
+    name: "secondName",
+    message: "Enter your second number: ",
+    type: "number"
+},
+{
+    name: "operator",
+    message: "Select one of the operator: ",
+    type: "list",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+
+}
+]);
+
+if(answers.operator === "Addition"){
+    console.log(answers.firstName + answers.secondName);
+} else if(answers.operator === "Subtraction"){
+    console.log(answers.firstName - answers.secondName);
+} else if(answers.operator === "Multiplication"){
+  console.log(answers.firstName * answers.secondName);
+} else if(answers.operator === "Division"){
+    console.log(answers.firstName / answers.secondName);
+} else{
+    console.log("Invalid operation");
+}
+
